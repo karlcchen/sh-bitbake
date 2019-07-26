fe18487b7ee364d75dc61375ee11391a7d500694
Karl Chen <kchen@sonicwall.com>
2019-07-25 22:50:13 -0700

    updated to call bb-diag.sh

--- bb-diag-clean-build.sh
+++ bb-diag-clean-build.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 #
-BB_PROJECT="diag-minimal"
-bitbake -c cleansstate ${BB_PROJECT} 2>&1 | tee clean-sstate-${BB_PROJECT}.log
+BUILD_TARGET="diag-minimal"
+LOG_FILE="${BUILD_TARGET}-clean-sstate.log"
+TEE_LOG="tee --append ${LOG_FILE}"
+rm -f ${LOG_FILE}
+bitbake -c cleansstate ${BB_PROJECT} 2>&1 | ${TEE_LOG}
 if [ $? -ne 0 ] ; then 
     echo "ERROR: bitbake -c cleansstate ${BB_PROJECT} failed!"
     exit 1
 fi 
-
-bitbake ${BB_PROJECT} $1 $2 $3 $4 $5 $6 $7 $8 $9 2>&1 | tee ${BB_PROJECT}.log
-if [ $? -ne 0 ] ; then 
-    echo "ERROR: bitbake ${BB_PROJECT} failed!"
-    exit 2
-fi
+#
+~/sh-bitbake/bb-diag.sh
 
