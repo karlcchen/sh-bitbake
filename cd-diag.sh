e43332d4b1a163da41ca0d516e27b5acc0fdd211
Karl Chen <kchen@sonicwall.com>
2019-07-30 14:01:38 -0700

    add cd-diag.sh

---
+++ cd-diag.sh
@@ -0,0 +1,23 @@
+#
+# cd-diag.sh
+#
+# source this from working directory to go to recipes-diag
+#
+if [ -d ./SonicCoreX ] ; then 
+    cd ./SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp/recipes-diag
+elif [ -d ./projects ] ; then 
+    cd ./projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp/recipes-diag
+elif [ -d ./lithium ] ; then 
+    cd ./lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp/recipes-diag
+elif [ -d ./sonicgit ] ; then 
+    cd ./sonicgit/soniccore/componentsx/meta-soniccorex-bsp/recipes-diag
+elif [ -d ./soniccore ] ; then 
+    cd ./soniccore/componentsx/meta-soniccorex-bsp/recipes-diag
+elif [ -d ./componentsx ] ; then 
+    cd ./componentsx/meta-soniccorex-bsp/recipes-diag
+elif [ -d ./meta-soniccorex-bsp ] ; then 
+    cd ./meta-soniccorex-bsp/recipes-diag
+elif [ -d ./recipes-diag ] ; then 
+    cd ./recipes-diag
+fi 
+
