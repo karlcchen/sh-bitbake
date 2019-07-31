0cdb17dd92e0bf3905dfc6aa2ff92d03a98c662c
Karl Chen <kchen@sonicwall.com>
2019-07-31 13:02:17 -0700

    rename and update bmap-tool-copy-wic2-sdd.sh to loma-prieta-bmap-wic.sh

--- loma-prieta-bmap-wic.sh
+++ loma-prieta-bmap-wic.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-~/bmap-tool-copy-wic2-sdd.sh -f build_output/deploy/images/loma-prieta/diag-minimal-loma-prieta.wic /dev/sdd
+#
+# 
+if [ "$1" = "" ] ; then 
+    echo -e "/nERROR: need dest device input: for example /dev/sdd/n"
+    exit 1 
+fi 
+~/sh-bitbake/bmap-tool-copy-wic2.sh -f build_output/deploy/images/loma-prieta/diag-minimal-loma-prieta.wic $1
+
