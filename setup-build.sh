b3b8656dec073fa78ae596318c1f18f08ab2e17b
Karl Chen <kchen@sonicwall.com>
2019-07-31 12:02:34 -0700

    update preparing for init-src, setup, build using one command

--- setup-build.sh
+++ setup-build.sh
@@ -3,25 +3,30 @@
 # setup-build.sh
 #
 
-BUILD_MACHINE="$1"
-BUILD_TARGET="$2"
+export BUILD_MACHINE="$1"
+export BUILD_TARGET="$2"
+export FILE_BUILD_COMPLETE=".build_complete"
 
-echo -e "\n BUILD_MACHINE=${BUILD_MACHINE}\n"
-echo -e " BUILD_TARGET=${BUILD_TARGET}\n"
-
-if [ ! -e ./setup.sh ] ; then 
-    echo
-    echo "ERROR: Must run it from directory that has ./setup.sh, fro example: SonicCoreX/" 
-    echo
-# do not exit as this file is sourced, also if "exited" and running in tmux/screen, it exit the current screen not "bash"  
-#    exit 1
+if [ "${BUILD_MACHINE}" = "" || "${BUILD_TARGET}" = "" ] ; then
+    echo -e "\nERROR: incomplete input found!\n"
 else
-    PROJECT=lithium MACHINE=${BUILD_MACHINE} PRODUCT="" CONF=dev source ./setup.sh build.${BUILD_TARGET}.${BUILD_MACHINE}
-    if [ $? -ne 0 ] ; then 
-	echo 
-	echo "ERROR: setup bitbake build environment: ${BUILD_TARGET}.${BUILD_MACHINE} failed!" 
-	echo
-#    exit 2
-    fi
-fi 
+    echo -e "\nBUILD_MACHINE=${BUILD_MACHINE}"
+    echo -e "BUILD_TARGET=${BUILD_TARGET}\n"
+    if [ ! -e ./setup.sh ] ; then 
+        echo -e "\nERROR: Must run it from directory that has ./setup.sh, for example: 'SonicCoreX'\n" 
+    # do not exit as this file is sourced, also if "exited" and running in tmux/screen, it exit the current screen not "bash"  
+    #    exit 1
+    else
+	pushd .
+	rm -f ${FILE_BUILD_COMPLETE}
+        PROJECT=lithium MACHINE=${BUILD_MACHINE} PRODUCT="" CONF=dev source ./setup.sh build.${BUILD_TARGET}.${BUILD_MACHINE}
+        if [ $? -ne 0 ] ; then 
+	    echo -e "\nERROR: setup bitbake build environment: ${BUILD_TARGET}.${BUILD_MACHINE} failed!\n" 
+	#    exit 2
+        fi
+	popd
+    fi 
+fi
+
+
 
--- setup-diag-loma-prieta.sh
+++ setup-diag-loma-prieta.sh
@@ -1,5 +1,11 @@
 #
 # setup-diag-loma-prieta.sh
 #
+<<<<<<< HEAD
 ~/sh-bitbake/setup-build.sh "loma-prieta" "diags"
+=======
+export BUILD_MACHINE="loma-prieta"
+export BUILD_TARGET="diag"
+source ~/sh-bitbake/setup-build.sh ${BUILD_MACHINE} ${BUILD_TARGET}
+>>>>>>> update for preparing init, setup and build at one command
 
