ada5f20df6cc7a334c53dc033e545843c4e9dcf3
Karl Chen <kchen@sonicwall.com>
2019-08-02 06:38:30 -0700

    update git-pull-sonicos-dev.sh with more messages

--- git-pull-sonicos-dev.sh
+++ git-pull-sonicos-dev.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+#
+# run from ./SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp
+#
 git pull sonicgit WIP/soniccorex/lithium/sonicos/6.5.4/dev
+if [ $? -ne 0 ] ; then 
+    echo -e "\nERROR: 'git pull sonicgit WIP/soniccorex/lithium/sonicos/6.5.4/dev' failed "\n
+    echo -e "\nTIPS: you should run this from ./SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp\n" 
+    exit 1
+fi 
