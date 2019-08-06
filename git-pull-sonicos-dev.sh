4cf1bd5c667f21a8af79f5a817bbde4f49819c0e
Karl Chen <kchen@sonicwall.com>
2019-08-06 12:15:32 -0700

    add --rebase to git-pull-sonicos-dev.sh

--- git-pull-sonicos-dev.sh
+++ git-pull-sonicos-dev.sh
@@ -2,7 +2,7 @@
 #
 # run from ./SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp
 #
-git pull sonicgit WIP/soniccorex/lithium/sonicos/6.5.4/dev
+git pull --rebase sonicgit WIP/soniccorex/lithium/sonicos/6.5.4/dev
 if [ $? -ne 0 ] ; then 
     echo -e "\nERROR: 'git pull sonicgit WIP/soniccorex/lithium/sonicos/6.5.4/dev' failed "\n
     echo -e "\nTIPS: you should run this from ./SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp\n" 
