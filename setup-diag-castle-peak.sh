067201650bc46ec9ddc5faf595ebfedc5040fb3f
Karl Chen <kchen@sonicwall.com>
2019-08-07 03:32:58 -0700

    add setup-diag-castle-peak.sh

---
+++ setup-diag-castle-peak.sh
@@ -0,0 +1,8 @@
+# 'source'd file, not for shell exec
+#
+# setup-diag-anderson-peak.sh
+#
+export BUILD_MACHINE="castle-peak"
+export BUILD_TARGET="diag"
+source ~/sh-bitbake/setup-build.sh ${BUILD_MACHINE} ${BUILD_TARGET}
+
