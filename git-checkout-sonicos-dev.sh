a50887587e2e3bf8eef78bfddf6592b71fc225ef
Karl Chen <kchen@sonicwall.com>
2019-08-06 19:17:26 -0700

    add pull from Marvell UMSD

--- git-checkout-sonicos-dev.sh
+++ git-checkout-sonicos-dev.sh
@@ -1,14 +1,32 @@
 #!/bin/bash
-BASE_DIR="SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp"
-DEV_BRANCH="remotes/m/WIP/soniccorex/lithium/sonicos/6.5.4/dev"
-cd ${BASE_DIR}
+BASE_DIR1="SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp"
+BASE_DIR2="SonicCoreX/projects/lithium/sonicgit/bsp/vendor-sdk/Marvell/UMSD"
+REMOTE_DEV_BRANCH="remotes/m/WIP/soniccorex/lithium/sonicos/6.5.4/dev"
+
+pushd .
+cd ${BASE_DIR1}
 if [ $? -ne 0 ] ; then 
-    echo "ERROR: 'cd ${BASE_DIR}' faied!"
+    echo -e "\nERROR: 'cd ${BASE_DIR1}' faied!\n"
     exit 1
 fi 
-git checkout --track ${DEV_BRANCH}
+git checkout --track ${REMOTE_DEV_BRANCH}
 if [ $? -ne 0 ] ; then 
-    echo "ERROR: 'git checkout --track ${DEV_BRANCH}' failed"
+    echo -e "\nERROR: 'git checkout --track ${REMOTE_DEV_BRANCH}' failed~\n"
     exit 2
+fi
+popd
+#
+#
+pushd .
+cd ${BASE_DIR2}
+if [ $? -ne 0 ] ; then 
+    echo -e "\nERROR: 'cd ${BASE_DIR2}' faied!\n"
+    exit 3
 fi 
+git checkout --track ${REMOTE_DEV_BRANCH}
+if [ $? -ne 0 ] ; then 
+    echo -e "\nERROR: 'git checkout --track ${REMOTE_DEV_BRANCH}' failed!\n"
+    exit 4
+fi
+popd
 
--- git-pull-sonicos-dev.sh
+++ git-pull-sonicos-dev.sh
@@ -2,9 +2,40 @@
 #
 # run from ./SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp
 #
-git pull --rebase sonicgit WIP/soniccorex/lithium/sonicos/6.5.4/dev
+BASE_DIR1="./SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp"
+BASE_DIR2="./SonicCoreX/projects/lithium/sonicgit/bsp/vendor-sdk/Marvell/UMSD"
+DEV_BRANCH="WIP/soniccorex/lithium/sonicos/6.5.4/dev"
+
+pushd .
+cd ${BASE_DIR1}
 if [ $? -ne 0 ] ; then 
-    echo -e "\nERROR: 'git pull sonicgit WIP/soniccorex/lithium/sonicos/6.5.4/dev' failed "\n
-    echo -e "\nTIPS: you should run this from ./SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp\n" 
+    echo -e "\nERROR: 'cd ${BASE_DIR1}' faied!\n"
     exit 1
 fi 
+echo -e "\nINFO: git pull from `pwd`\n"
+git pull --rebase sonicgit ${DEV_BRANCH}
+if [ $? -ne 0 ] ; then 
+    pwd
+    echo -e "\nERROR: 'git pull --rebase sonicgit ${DEV_BRANCH}' failed!\n"
+    echo -e "\nTIPS: you should run this from ${BASE_DIR1}\n" 
+    exit 2
+fi 
+popd
+#
+#
+pushd .
+cd ${BASE_DIR2}
+if [ $? -ne 0 ] ; then 
+    echo -e "\nERROR: 'cd ${BASE_DIR2}' faied!\n"
+    exit 3
+fi 
+echo -e "\nINFO: git pull from `pwd`\n"
+git pull --rebase sonicgit ${DEV_BRANCH}
+if [ $? -ne 0 ] ; then 
+    pwd
+    echo -e "\nERROR: 'git pull --rebase sonicgit ${DEV_BRANCH}' failed!\n"
+    echo -e "\nTIPS: you should run this from ${BASE_DIR2}\n" 
+    exit 4
+fi 
+popd
+
