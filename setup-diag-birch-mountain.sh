0c6e368c88c19c9dfd909efa34443c87510fd872
Karl Chen <kchen@sonicwall.com>
2019-08-07 09:25:25 -0700

    add setup-diag-birch-mountain.sh setup-diag-mammoth-mountain.sh setup-diag-mount-ritter.sh setup-diag-palomar-mountain.sh

---
+++ setup-diag-birch-mountain.sh
@@ -0,0 +1,8 @@
+# 'source'd file, not for shell exec
+#
+# setup-diag-anderson-peak.sh
+#
+export BUILD_MACHINE="birch-mountain"
+export BUILD_TARGET="diag"
+source ~/sh-bitbake/setup-build.sh ${BUILD_MACHINE} ${BUILD_TARGET}
+
