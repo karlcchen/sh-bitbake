5d759dbef2d07a0ac125f734116f8c84ece1b188
Karl Chen <kchen@sonicwall.com>
2019-09-17 15:23:26 -0700

    update cd-deploy-images.sh

--- cd-deploy-images.sh
+++ cd-deploy-images.sh
@@ -1,4 +1,20 @@
+# to be sourced
 #
 # cd-deploy-images.sh
 #
-source cdjump "SonicCoreX" "./SonicCoreX/build.diag.castle-peak/build_output/deploy/images"
+if [ "$1" == "" ] ; then 
+    printf "\nERROR: Need build machine name as 1st argument: for example \"anderson-peak\"\n\n"
+    printf "For example:\n\t ./cd-deplay-image.sh anderson-peak\n\n"
+    return 1
+fi 
+
+BUILD_MACHINE="$1"
+source cdjump "SonicCoreX" "./SonicCoreX/build.diag.${BUILD_MACHINE}/build_output/deploy/images"
+
+if [ -d ${BUILD_MACHINE} ] ; then 
+    cd ${BUILD_MACHINE}
+else
+    printf "\nERROR: cannot cd to directory: %s\n" "${BUILD_MACHINE}"
+    return 2
+fi 
+
