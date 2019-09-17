9b0908ea6c1571eac0cb3c6c298a7cc0dc64ed87
Karl Chen <kchen@sonicwall.com>
2019-09-17 15:35:30 -0700

    modified:   cd-base-dir.sh

--- cd-base-dir.sh
+++ cd-base-dir.sh
@@ -3,5 +3,5 @@
 #
 # source this from working directory to go to recipes-diag
 #
-source "SonicCoreX" "./SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp"
+source cdjump "SonicCoreX" "./SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/meta-soniccorex-bsp"
 
