95ef0f82e7184ec8db5134113143791ebac8657c
Karl Chen <kchen@sonicwall.com>
2019-09-17 13:59:54 -0700

    new file:   cd-recipes-core-image.sh

---
+++ cd-recipes-core-image.sh
@@ -0,0 +1,6 @@
+# file to be sourced
+#
+# cd-recipes-core-images.sh
+#
+source cdjump "SonicCoreX" "./SonicCoreX/projects/lithium/sonicgit/soniccore/componentsx/soniccorex-components/recipes-core/images"
+
