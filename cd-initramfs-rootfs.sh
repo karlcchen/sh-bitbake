942a03721ffefddfba10d4ac0681888c5a0981c9
Karl Chen <kchen@sonicwall.com>
2019-09-17 17:26:55 -0700

    modified:   cd-initramfs-rootfs.sh

--- cd-initramfs-rootfs.sh
+++ cd-initramfs-rootfs.sh
@@ -1,15 +1,18 @@
 #
-# cd-rootfs.sh 
+# cd-rootfs-initramfs.sh 
 #
-
+SCRIPT_NAME="cd-rootfs-initramfs.sh"
+ 
 if [ "$1" == "" ] ; then 
     printf "\nERROR: Need build machine name as 1st argument: for example \"anderson-peak\"\n\n"
-    printf "For example:\n\t ./cd-rootfs.sh anderson-peak\n\n"
+    printf "For example:\n\t ./%s %s\n\n" "${SCRIPT_NAME}" "anderson-peak"
     return 1
 fi 
-
+#
 BUILD_MACHINE="$1"
-BUILD_TARGET="diag"
-INITRAMFS_IMAGE_NAME="diag-kchen-image-initramfs"
 BUILD_MACHINE1="`echo ${BUILD_MACHINE} | sed -e 's/-/_/g'`"
-source cdjump "SonicCoreX" "./SonicCoreX/build.${BUILD_TARGET}.${BUILD_MACHINE}/build_output/work/${BUILD_MACHINE1}-soniccorex-linux/${INITRAMFS_IMAGE_NAME}/1.0-r0/rootfs"
+BUILD_TARGET="diag"
+ROOTFS_IMAGE_NAME="diag-kchen-image-initramfs"
+DEST_DIR="./SonicCoreX/build.${BUILD_TARGET}.${BUILD_MACHINE}/build_output/work/${BUILD_MACHINE1}-soniccorex-linux/${ROOTFS_IMAGE_NAME}/1.0-r0/rootfs"
+#
+source cdjump "SonicCoreX" "${DEST_DIR}"
