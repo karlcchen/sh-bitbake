e1a0b91b8e8adcafbd2243ec7e2d281ef8f92b96
Karl Chen <kchen@sonicwall.com>
2019-09-17 17:27:23 -0700

    new file:   bb-diag-clean.sh

---
+++ bb-diag-clean.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+BB_PROJECT="diag-minimal"
+LOG_FILE="${BB_PROJECT}-clean-sstate.log"
+TEE_LOG="tee --append ${LOG_FILE}"
+rm -f ${LOG_FILE}
+bitbake -c cleansstate ${BB_PROJECT} 2>&1 | ${TEE_LOG}
+if [ $? -ne 0 ] ; then 
+    echo "ERROR: bitbake -c cleansstate ${BB_PROJECT} failed!"
+    exit 1
+fi 
+bitbake -c cleanall diag 2>&1 | ${TEE_LOG}
+if [ $? -ne 0 ] ; then 
+    echo "ERROR: bitbake -c cleanall diag failed!"
+    exit 1
+fi 
