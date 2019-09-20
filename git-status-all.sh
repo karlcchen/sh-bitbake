1601e0c648ed13fe2cbe314e4cc6eb99e00fe490
Karl Chen <kchen@sonicwall.com>
2019-09-19 20:21:49 -0700

    update git-status-all.sh

--- git-status-all.sh
+++ git-status-all.sh
@@ -2,8 +2,19 @@
 #
 #
 
-# remove ".git" at ned of pathname
-CD_LIST=`dname ".git" | sed "s/\.git//g"`
+if [ ! "${1}" = "" ] ; then 
+    cd $1
+    if [ $? -ne 0 ] ; then 
+        printf "\nERROR: cd \"%s\" failed!\n" "${1}"
+        exit 1
+    fi 
+fi 
+
+CD_LIST="`~/sh-bitbake/git-dirs.sh`"
+if [ $? -ne 0 ] ; then 
+    printf "\nERROR: ~/sh-bitbake/git-dirs.sh %s failed!\n" "${1}"
+    exit 2
+fi 
 
 # for debug only 
 #echo 
@@ -11,6 +22,7 @@ CD_LIST=`dname ".git" | sed "s/\.git//g"`
 #echo 
 #
 N_COUNT=0
+N_FOUND=0
 for cd_name in ${CD_LIST}
 do 
     N_COUNT=$((N_COUNT+1))
@@ -18,17 +30,21 @@ do
     cd ${cd_name} 
     if [ $? -ne 0 ] ; then 
         printf "\n ERROR: #%d, cd %s failed!\n" ${N_COUNT} "${cd_name}"
-        exit 2 
+        exit 3 
     fi 
     GIT_STATUS="`git status`"
     if [ $? -ne 0 ] ; then 
         printf "\nERROR: #%d, \"git status\" failed at DIR: %s\n" ${N_COUNT} "${cd_name}"
-        exit 3 
+        exit 4 
     fi 
     echo "${GIT_STATUS}" | grep "modified" >/dev/null
     if [ $? -eq 0 ] ; then 
         printf "\n=== #%d, DIR %s ===\n" ${N_COUNT} "${cd_name}"
         echo "${GIT_STATUS}"               
+        N_FOUND=$((N_FOUND+1))
     fi 
     popd >/dev/null
 done
+
+printf "\n= git-status-all.sh DONE ===\n"
+printf " %d modified git repo found\n\n" ${N_FOUND}
