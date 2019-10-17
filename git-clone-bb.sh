763413b7898388e8dbb4987a22c96982d0b9c1e4
Karl Chen <kchen@sonicwall.com>
2019-10-16 20:35:53 -0700

    add git-clone-bb.sh

---
+++ git-clone-bb.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+# git-clone-bb.sh
+# 
+# 
+# GIT_BRANCH=""
+GIT_SRC2="https://sonicgit.eng.sonicwall.com/kchen/bb.git"
+GIT_SRC="git@sonicgit.eng.sonicwall.com:kchen/bb.git"
+#
+# git clone ${GIT_SRC} --branch ${GIT_BRANCH}
+git clone ${GIT_SRC} 
