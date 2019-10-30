fd67be0e0a6463e4a90e1e6b082a3652aec0093b
Karl Chen <kchen@sonicwall.com>
2019-10-30 16:17:16 +0800

    new git-branch-all.sh

---
+++ git-branch-all.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+#
+
+if [ ! "${1}" = "" ] ; then 
+    cd $1
+    if [ $? -ne 0 ] ; then 
+        printf "\nERROR: cd \"%s\" failed!\n" "${1}"
+        exit 1
+    fi 
+fi 
+
+CD_LIST="`~/sh-bitbake/git-dirs.sh`"
+if [ $? -ne 0 ] ; then 
+    printf "\nERROR: ~/sh-bitbake/git-dirs.sh %s failed!\n" "${1}"
+    exit 2
+fi 
+
+# for debug only 
+#echo 
+#echo "${CD_LIST}"
+#echo 
+#
+N_COUNT=0
+CUR_DIR="`pwd`"
+COMMIT_VER_ID_FNAME="${CUR_DIR}/prj-ver-id.txt"
+COMMIT_BRANCH_FNAME="${CUR_DIR}/prj-branch.txt"
+rm -f ${COMMIT_VER_ID_FNAME}
+rm -f ${COMMIT_BRANCH_FNAME}
+for cd_name in ${CD_LIST}
+do 
+    N_COUNT=$((N_COUNT+1))
+    pushd . >/dev/null
+    cd ${cd_name} 
+    if [ $? -ne 0 ] ; then 
+        printf "\n ERROR: #%d, cd %s failed!\n" ${N_COUNT} "${cd_name}"
+        exit 3 
+    fi 
+    git branch 
+    if [ $? -ne 0 ] ; then 
+        printf "\nERROR: #%d, \"git branch\" failed at DIR: %s\n" ${N_COUNT} "${cd_name}"
+        exit 4 
+    fi 
+    printf "%d %s %s %s\n" ${N_COUNT} "${cd_name}" "${GIT_COMMIT_ID}" "${GIT_COMMIT_BRANCH}"
+    popd >/dev/null
+done
+
+printf "\n=====  git-version-all.sh DONE  =====\n"
+ls -l prj*.txt
