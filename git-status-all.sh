f7b5b6c7fbe6c327fbbb2290032e081de31f6603
Karl Chen <kchen@sonicwall.com>
2020-03-17 12:55:53 -0700

    updates

--- git-status-all.sh
+++ git-status-all.sh
@@ -13,7 +13,7 @@ if [ ! "${1}" = "" ] ; then
     fi 
 fi 
 
-CD_LIST="`${BB_DIR}/git-dirs.sh`"
+DIR_LIST="`${BB_DIR}/git-dirs.sh`"
 if [ $? -ne 0 ] ; then 
     printf "\nERROR: ${BB_DIR}/git-dirs.sh %s failed!\n" "${1}"
     exit 2
@@ -21,26 +21,26 @@ fi
 
 # for debug only 
 #echo 
-#echo "${CD_LIST}"
+#printf "DEBUG: DIR_LIST:%s\n" "${DIR_LIST}"
 #echo 
 #
 N_COUNT=0
 N_FOUND=0
-for cd_name in ${CD_LIST}
+for dir_name in ${DIR_LIST}
 do 
     N_COUNT=$((N_COUNT+1))
     pushd . >/dev/null
-    cd ${cd_name} 
+    cd ${dir_name} 
     if [ $? -ne 0 ] ; then 
-        printf "\n ERROR: #%d, cd %s failed!\n" ${N_COUNT} "${cd_name}"
+        printf "\n ERROR: #%d, cd %s failed!\n" ${N_COUNT} "${dir_name}"
         exit 3 
     fi 
     GIT_STATUS="`git status`"
     if [ $? -ne 0 ] ; then 
-        printf "\nERROR: #%d, \"git status\" failed at DIR: %s\n" ${N_COUNT} "${cd_name}"
+        printf "\nERROR: #%d, \"git status\" failed at DIR: %s\n" ${N_COUNT} "${dir_name}"
         exit 4 
     fi 
-    printf "\n=== #%d, DIR %s ===\n" ${N_COUNT} "${cd_name}"
+    printf "\n=== #%d, DIR %s ===\n" ${N_COUNT} "${dir_name}"
     echo "${GIT_STATUS}"               
     N_FOUND=$((N_FOUND+1))
     popd >/dev/null
--- git-status-modified.sh
+++ git-status-modified.sh
@@ -15,7 +15,7 @@ if [ ! "${1}" = "" ] ; then
     fi 
 fi 
 
-CD_LIST="`${BB_DIR}/git-dirs.sh`"
+DIR_LIST="`${BB_DIR}/git-dirs.sh`"
 if [ $? -ne 0 ] ; then 
     printf "\nERROR: ${BB_DIR}/git-dirs.sh \"%s\" failed!\n" "${1}"
     exit 2
@@ -23,34 +23,34 @@ fi
 
 # for debug only 
 #echo 
-#echo "${CD_LIST}"
+#echo "${DIR_LIST}"
 #echo 
 #
 N_COUNT=0
 N_FOUND=0
-for cd_name in ${CD_LIST}
+for dir_name in ${DIR_LIST}
 do 
     N_COUNT=$((N_COUNT+1))
     pushd . >/dev/null
-    cd ${cd_name} 
+    cd ${dir_name} 
     if [ $? -ne 0 ] ; then 
-        printf "\n ERROR: #%d, cd \"%s\" failed!\n" ${N_COUNT} "${cd_name}"
+        printf "\n ERROR: #%d, cd \"%s\" failed!\n" ${N_COUNT} "${dir_name}"
         exit 3 
     fi 
     GIT_STATUS="`git status`"
     if [ $? -ne 0 ] ; then 
-        printf "\nERROR: #%d, \"git status\" failed at DIR: \"%s\"\n" ${N_COUNT} "${cd_name}"
+        printf "\nERROR: #%d, \"git status\" failed at DIR: \"%s\"\n" ${N_COUNT} "${dir_name}"
         exit 4 
     fi 
     echo "${GIT_STATUS}" | grep "${LOOK_FOR_STR1}" >/dev/null
     if [ $? -eq 0 ] ; then 
-        printf "\n=== #%d, git DIR \"%s\", search \"%s\" ===\n" ${N_COUNT} "${cd_name}" "${LOOK_FOR_STR1}"
+        printf "\n=== #%d, git DIR \"%s\", search \"%s\" ===\n" ${N_COUNT} "${dir_name}" "${LOOK_FOR_STR1}"
         echo "${GIT_STATUS}"               
         N_FOUND=$((N_FOUND+1))
     else 
         echo "${GIT_STATUS}" | grep "${LOOK_FOR_STR2}" >/dev/null
         if [ $? -eq 0 ] ; then 
-            printf "\n=== #%d, git DIR \"%s\", search \"%s\" ===\n" ${N_COUNT} "${cd_name}" "${LOOK_FOR_STR2}"
+            printf "\n=== #%d, git DIR \"%s\", search \"%s\" ===\n" ${N_COUNT} "${dir_name}" "${LOOK_FOR_STR2}"
             echo "${GIT_STATUS}"               
             N_FOUND=$((N_FOUND+1))
         fi 
